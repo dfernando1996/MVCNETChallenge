f1202f8d53f3dc9a2f386326d7e04786d4f30533
Juan Pinilla <Juan.Pinilla@endava.com>
2019-03-01 16:27:11 -0500

    Project Finished, added Dates and the new features

---
+++ NetDos/Models/Modelo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    
+    public class Modelo
+    {
+        
+    }
+
+    public class Rootobject
+    {
+        private Dictionary<string, info[]> _near_earth_objects = new Dictionary<string, info[]>();
+        public Links links { get; set; }
+        public int element_count { get; set; }
+        public Dictionary<string, info[]> near_earth_objects
+        {
+            get
+            {
+                return _near_earth_objects;
+            }
+            set
+            {
+                /*
+                 Esto asegura que el diccionario implementado siempre mantenga un valor diferente de nulo
+                 */
+                if (value != null)
+                    this._near_earth_objects = value;
+                else
+                    _near_earth_objects = new Dictionary<string, info[]>();
+            }
+        }
+    }
+
+    public class Links
+    {
+        public string next { get; set; }
+        public string prev { get; set; }
+        public string self { get; set; }
+    }
+
+    public class info
+    {
+        public Links1 links { get; set; }
+        public string id { get; set; }
+        public string neo_reference_id { get; set; }
+        public string name { get; set; }
+        public string nasa_jpl_url { get; set; }
+        public float absolute_magnitude_h { get; set; }
+        public Estimated_Diameter estimated_diameter { get; set; }
+        public bool is_potentially_hazardous_asteroid { get; set; }
+        public Close_Approach_Data[] close_approach_data { get; set; }
+        public bool is_sentry_object { get; set; }
+    }
+
+    public class Links1
+    {
+        public string self { get; set; }
+    }
+
+    public class Estimated_Diameter
+    {
+        public Kilometers kilometers { get; set; }
+        public Meters meters { get; set; }
+        public Miles miles { get; set; }
+        public Feet feet { get; set; }
+    }
+
+    public class Kilometers
+    {
+        public float estimated_diameter_min { get; set; }
+        public float estimated_diameter_max { get; set; }
+    }
+
+    public class Meters
+    {
+        public float estimated_diameter_min { get; set; }
+        public float estimated_diameter_max { get; set; }
+    }
+
+    public class Miles
+    {
+        public float estimated_diameter_min { get; set; }
+        public float estimated_diameter_max { get; set; }
+    }
+
+    public class Feet
+    {
+        public float estimated_diameter_min { get; set; }
+        public float estimated_diameter_max { get; set; }
+    }
+
+    public class Close_Approach_Data
+    {
+        public string close_approach_date { get; set; }
+        public long epoch_date_close_approach { get; set; }
+        public Relative_Velocity relative_velocity { get; set; }
+        public Miss_Distance miss_distance { get; set; }
+        public string orbiting_body { get; set; }
+    }
+
+    public class Relative_Velocity
+    {
+        public string kilometers_per_second { get; set; }
+        public string kilometers_per_hour { get; set; }
+        public string miles_per_hour { get; set; }
+    }
+
+    public class Miss_Distance
+    {
+        public string astronomical { get; set; }
+        public string lunar { get; set; }
+        public string kilometers { get; set; }
+        public string miles { get; set; }
+    }
+
+
+
+
+}
---
+++ NetDos/NetDosUno/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using NetDosUno.Models;
+using Newtonsoft.Json;
+using static Models.Modelo;
+
+namespace NetDosUno.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Asteroids()
+        { 
+          
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Asteroids(string start, string end, [Bind("near_earth_object")] Rootobject rootObj)
+        {
+            //Rootobject modelo;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://api.nasa.gov/neo/rest/v1/feed?start_date=" + start + "&end_date=" + end + "&api_key=GzRydzlaSEfI60xIHFvraQcNVEqw5Dc4Pr8PHZfU");
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+                rootObj = JsonConvert.DeserializeObject<Rootobject>(json);
+                return View(rootObj);
+            }
+
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
